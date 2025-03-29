a1a5f4c219e87e7b1b88416cd1225de5ebe58e50
ma <matianci888@163.com>
2025-03-29 12:03:15 +0800

    header组件的静态搭建

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,44 +1,35 @@
 <script setup>
-import CommonAside from '@/components/CommonAside.vue';
+   import CommonAside from '@/components/CommonAside.vue'
+   import CommonHeader from '@/components/CommonHeader.vue'
 </script>
 
-
 <template>
-  <div class="common-layout">
-    <el-container class="lay-container">
-        <!-- 自定义的左侧的组件 -->
-         <common-aside/>
-          <el-container>
+   <div class="common-layout">
+      <el-container class="lay-container">
+         <!-- 自定义的左侧的组件 -->
+         <common-aside />
+         <el-container>
             <el-header class="el-header">
-              <common-header>
-
-              </common-header>
+               <common-header />
+               <!-- </common-header> -->
             </el-header>
-            <el-main class="right-main">
-                main
-            </el-main>
-          </el-container>
-        
-    </el-container>
-
-
-  </div>
+            <el-main class="right-main"> main </el-main>
+         </el-container>
+      </el-container>
+   </div>
 </template>
 
 <style scoped lang="less">
-  .common-layout{
-    width: 100%;
-    height: 100vh;
-  }
-  .lay-container{
-    width: 100%;
-    height: 100%;
-  }
-  .el-header{
-    background-color: #333;
-    // width: 100%;
-  }
-
-
-
+   .common-layout {
+      width: 100%;
+      height: 100vh;
+   }
+   .lay-container {
+      width: 100%;
+      height: 100%;
+   }
+   .el-header {
+      background-color: #333;
+      // width: 100%;
+   }
 </style>
