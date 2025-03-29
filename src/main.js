e0e0de6781de104b8d79d3a218979b20775d9673
ma <matianci888@163.com>
2025-03-29 12:51:14 +0800

    引入pinia

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
-import { createApp } from 'vue';
-import "@/assets/less/index.less";
-import App from './App.vue';
+import { createApp } from 'vue'
+import '@/assets/less/index.less'
+import App from './App.vue'
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
 
-import router from "./router/index.js";
+import router from './router/index.js'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import { createPinia } from 'pinia'
+const pinia = createPinia()
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+   app.component(key, component)
 }
 
-app.use(router);
-// app.use(ElementPlus);
-app.mount('#app');
+app.use(router)
+app.use(pinia)
+app.mount('#app')
 
 // createApp(App).mount('#app')
