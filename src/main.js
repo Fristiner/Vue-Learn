c35c6df57ae59ec2dfc64d7df940b8d4aebddaa3
ma <matianci888@163.com>
2025-03-29 09:24:11 +0800

    注册所有图标引入

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import App from './App.vue';
 // import 'element-plus/dist/index.css'
 
 import router from "./router/index.js";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-const app = createApp(App);
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(router);
 // app.use(ElementPlus);
